85b953ef0999be3479cc3bf66cc2209c6e032ce8
Baltazár Radics <baltazar.radics@gmail.com>
2021-06-09 21:15:19 +0200

    Calculate mention offsets as utf-16
    This way they will bridge correctly to matrix, and appear correctly in
    facebook's mobile clients (but not the web).

--- mautrix_facebook/formatter/from_matrix.py
+++ mautrix_facebook/formatter/from_matrix.py
@@ -101,8 +101,9 @@ async def matrix_to_facebook(content: TextMessageEventContent, room_id: RoomID, 
                     fbid = puppet.fbid
                 else:
                     continue
-            mentions.append(Mention(user_id=str(fbid), offset=mention.offset,
-                                    length=mention.length))
+            offset = len(text[:mention.offset].encode('utf-16')) // 2 - 1
+            length = len(text[mention.offset:mention.offset+mention.length].encode('utf-16')) // 2 - 1
+            mentions.append(Mention(user_id=str(fbid), offset=offset, length=length))
     else:
         text = content.body
     return SendParams(text=text, mentions=mentions, reply_to=reply_to)
